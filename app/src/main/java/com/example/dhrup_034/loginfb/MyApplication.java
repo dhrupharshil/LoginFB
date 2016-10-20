47f6ca2b66a98cb322d0d8cb2ee53e4d2c43ed51
dhrupharshil <dhrup.harshil@gmail.com>
2016-10-20 17:30:06 +0530

    android updated code  for login with facebook

--- app/src/main/java/com/example/dhrup_034/loginfb/MyApplication.java
+++ app/src/main/java/com/example/dhrup_034/loginfb/MyApplication.java
@@ -28,6 +28,7 @@ public class MyApplication extends Application {
                     "net.simplifiedcoding.androidlogin",
                     PackageManager.GET_SIGNATURES);
             for (Signature signature : info.signatures) {
+
                 MessageDigest md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
                 Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
